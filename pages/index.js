9aa0327fa6bae2b76a2142591024dd99f448a934
Mauricio Macedo <plekeapp.dev@gmail.com>
2022-05-30 21:11:17 -0300

    altera conteudo

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Abassis Finance Manager</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Abassis Finance Manager has been discontinued" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <a href="https://www.filesculptor.com/convert-csv-to-excel/">How to Convert CSV to Excel Manually or Automatically</a>
         </p>
       </main>
 
-      <Footer />
     </div>
   )
 }
